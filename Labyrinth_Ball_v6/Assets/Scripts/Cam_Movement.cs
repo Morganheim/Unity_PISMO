a5dccd27591ca7188982243235d4a42a2b77fcd7
Morganheim <timon.popovic@gmail.com>
2021-09-19 13:55:57 +0200

    LB_v6_2

--- Labyrinth_Ball_v6/Assets/Scripts/Cam_Movement.cs
+++ Labyrinth_Ball_v6/Assets/Scripts/Cam_Movement.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Cam_Movement : MonoBehaviour
 {
-    
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
 }
