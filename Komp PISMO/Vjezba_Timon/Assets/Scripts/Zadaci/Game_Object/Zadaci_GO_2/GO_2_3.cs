cca0da31c95513f7c335031acb74ce7261359bd3
Morganheim <timon.popovic@gmail.com>
2021-09-20 21:13:35 +0200

    Update GO_2_3.cs

--- Komp PISMO/Vjezba_Timon/Assets/Scripts/Zadaci/Game_Object/Zadaci_GO_2/GO_2_3.cs
+++ Komp PISMO/Vjezba_Timon/Assets/Scripts/Zadaci/Game_Object/Zadaci_GO_2/GO_2_3.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class GO_2_3 : MonoBehaviour
 {
-    public GameObject kocka;
     public GameObject sfera;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject == sfera)
+        if (collision.gameObject.name == "Sfera")
         {
-            Destroy(sfera.gameObject);
+            Destroy(collision.gameObject);
         }
     }
 }
