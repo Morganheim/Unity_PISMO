adf66a57158ba8c751b5ee579deadd2efe608409
Morganheim <timon.popovic@gmail.com>
2021-09-23 13:58:55 +0200

    IP_UI_v2

--- Komp PISMO/Vjezba_Timon/Assets/Scripts/Zadaci/Iso_game_22_09/GameManager_iso.cs
+++ Komp PISMO/Vjezba_Timon/Assets/Scripts/Zadaci/Iso_game_22_09/GameManager_iso.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager_iso : MonoBehaviour
 {
+    public GameObject[] sceneObjekti;
+
     public int score;
     public int scoreCounter;
     public int life = 5;
+
+    public Text lifeText;
+    public Text scoreText;
+
+    private void Start()
+    {
+        scoreText.text = "Score: " + score;
+        lifeText.text = "Life: " + life + "/5";
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
+            PauseGame();
         }
     }
 
@@ -20,18 +32,42 @@ public class GameManager_iso : MonoBehaviour
     {
         score++;
         scoreCounter++;
+        scoreText.text = "Score: " + score;
         Debug.Log("<color=green>Score je: " + score + "</color> i <color=red> life je: " + life + "</color>");
     }
 
     public void MakniLife()
     {
         life--;
-
+        lifeText.text = "Life: " + life + "/5";
         Debug.Log("<color=green>Score je: " + score + "</color> i <color=red> life je: " + life + "</color>");
         if (life <= 0)
         {
             Debug.Log("<color=red>Izgubio si. Konacni score je: " + score + "</color>");
             Destroy(this.gameObject);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void StartGame()
+    {
+        for(int i = 0; i < sceneObjekti.Length; i++)
+        {
+            sceneObjekti[i].SetActive(true);
         }
     }
 }
